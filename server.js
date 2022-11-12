96a162a62d0f671d5982311271538e31e7a4c605
Rebeckaspolander <rebeckaspolander@yahoo.se>
2022-11-12 13:49:30 +0100

    Basic GET.

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import express from "express";
 
 const app = express();
+//When you go to this path(endpoint) you will get a request and response.
+//This sends just plain text
+// app.get("/login", (req, res)=>{
+//         res.json("login endpoint");
+// })
+
+//You can change the end point to "what ever you want" ("/your end point" endpoint).
+app.get("/login",(req, res)=>{
+        res.json({
+                username: "admin"
+        });
+})
 
 const server = app.listen(
     process.env.PORT || 3000,
